aa3f44e1db66e658b261df5777a912ea88edbfa9
PedroHenr1 <pedrohenrique1258@hotmail.com>
2021-03-11 22:02:20 -0300

    incluidos comentarios

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 
 const ProductsController = require ('./controllers/ProductController') 
 
+//Routes
 routes.get("/products", ProductsController.index);
 routes.get("/products/:id", ProductsController.show);
 routes.post("/products", ProductsController.store);
